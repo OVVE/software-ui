f24f2049ef88be41ade9cb4089a57b70ccd8c9ad
zmk5 <zkakish@gmail.com>
2020-04-02 11:52:17 -0700

    syntax fixes and type hinting added

--- display/ui_settings.py
+++ display/ui_settings.py
@@ -1,26 +1,27 @@
+from typing import Tuple, Optional
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
 
 class TextSetting:
-    def __init__(self, fontName, fontSize, bold):
+    def __init__(self, fontName: str, fontSize: int, bold: bool):
         if bold:
             self.font = QFont(fontName, fontSize, QFont.Bold)
-        else:
-            self.font = QFont(fontName, fontSize)
+
+        self.font = QFont(fontName, fontSize)
 
 
 #TODO: Abstract text placement
 class FancyButtonSettings:
     def __init__(self,
-                 labelSetting=TextSetting("Times", 20, True),
-                 valueSetting=TextSetting("Times", 36, True),
-                 unitSetting=TextSetting("Times", 18, False),
-                 fillColor='#d2fcdc',
-                 labelColor='#3ed5f0',
-                 valueColor=Qt.black,
-                 unitColor=Qt.gray,
-                 default_size=(150, 80)):
+                 labelSetting: TextSetting = TextSetting("Times", 20, True),
+                 valueSetting: TextSetting = TextSetting("Times", 36, True),
+                 unitSetting: TextSetting = TextSetting("Times", 18, False),
+                 fillColor: str ='#d2fcdc',
+                 labelColor: str ='#3ed5f0',
+                 valueColor: Qt.GlobalColor = Qt.black,
+                 unitColor: Qt.GlobalColor = Qt.gray,
+                 default_size: Tuple[int, int] = (150, 80)):
 
         self.labelFont = labelSetting.font
         self.valueFont = valueSetting.font
@@ -31,29 +32,31 @@ class FancyButtonSettings:
         self.unitColor = unitColor
         self.default_size = default_size
 
-    def getFillBrush(self):
+    def getFillBrush(self) -> QBrush:
         return QBrush(QColor(self.fillColor))
 
-    def getLabelPen(self):
+    def getLabelPen(self) -> QPen:
         return QPen(QColor(self.labelColor))
 
-    def getValuePen(self):
+    def getValuePen(self) -> QPen:
         return QPen(QColor(self.valueColor))
 
-    def getUnitPen(self):
+    def getUnitPen(self) -> QPen:
         return QPen(QColor(self.unitColor))
 
-    def getLabelCoords(self, size=None):
+    def getLabelCoords(self, size=None) -> Tuple[int, int]:
         use_size = self.default_size if size is None else size
         return int(use_size[0] / 4), \
                int(use_size[1] * 1 / 5)
 
-    def getValueCoords(self, size=None):
+    def getValueCoords(
+            self, size: Optional[Tuple[int, int]] = None) -> Tuple[int, int]:
         use_size = self.default_size if size is None else size
         return int(use_size[0] / 4), \
                int(use_size[1] * 3 / 5)
 
-    def getUnitCoords(self, size=None):
+    def getUnitCoords(
+        self, size: Optional[Tuple[int, int]] = None) -> Tuple[int, int]:
         use_size = self.default_size if size is None else size
         return int(use_size[0] / 4), \
                int(use_size[1] * 9 / 10)
@@ -61,23 +64,24 @@ class FancyButtonSettings:
 
 class SimpleButtonSettings:
     def __init__(self,
-                 valueSetting=TextSetting("Times", 20, True),
-                 fillColor='#d2fcdc',
-                 valueColor=Qt.black,
-                 default_size=(150, 25)):
+                 valueSetting: TextSetting = TextSetting("Times", 20, True),
+                 fillColor: str = '#d2fcdc',
+                 valueColor: Qt.GlobalColor = Qt.black,
+                 default_size: Tuple[int, int] = (150, 25)):
 
         self.valueFont = valueSetting.font
         self.fillColor = fillColor
         self.valueColor = valueColor
         self.default_size = default_size
 
-    def getFillBrush(self):
+    def getFillBrush(self) -> QBrush:
         return QBrush(QColor(self.fillColor))
 
-    def getValuePen(self):
+    def getValuePen(self) -> QPen:
         return QPen(QColor(self.valueColor))
 
-    def getValueCoords(self, size=None):
+    def getValueCoords(
+            self, size: Optional[Tuple[int, int]] = None) ->Tuple[int, int]:
         use_size = self.default_size if size is None else size
         return int(use_size[0] / 4), \
                int(use_size[1] * 4 / 5)
@@ -85,14 +89,14 @@ class SimpleButtonSettings:
 
 class DisplayRectSettings:
     def __init__(self,
-                 labelSetting=TextSetting("Times", 20, True),
-                 valueSetting=TextSetting("Times", 36, True),
-                 unitSetting=TextSetting("Times", 18, False),
-                 fillColor='#c4dbff',
-                 labelColor='#3ed5f0',
-                 valueColor=Qt.black,
-                 unitColor=Qt.gray,
-                 default_size=(150, 80)):
+                 labelSetting: TextSetting = TextSetting("Times", 20, True),
+                 valueSetting: TextSetting = TextSetting("Times", 36, True),
+                 unitSetting: TextSetting = TextSetting("Times", 18, False),
+                 fillColor: str = '#c4dbff',
+                 labelColor: str = '#3ed5f0',
+                 valueColor: Qt.GlobalColor = Qt.black,
+                 unitColor: Qt.GlobalColor = Qt.gray,
+                 default_size: Tuple[int, int] = (150, 80)):
         self.labelFont = labelSetting.font
         self.valueFont = valueSetting.font
         self.unitFont = unitSetting.font
@@ -102,29 +106,32 @@ class DisplayRectSettings:
         self.unitColor = unitColor
         self.default_size = default_size
 
-    def getFillBrush(self):
+    def getFillBrush(self) -> QBrush:
         return QBrush(QColor(self.fillColor))
 
-    def getLabelPen(self):
+    def getLabelPen(self) -> QPen:
         return QPen(QColor(self.labelColor))
 
-    def getValuePen(self):
+    def getValuePen(self) -> QPen:
         return QPen(QColor(self.valueColor))
 
-    def getUnitPen(self):
+    def getUnitPen(self) -> QPen:
         return QPen(QColor(self.unitColor))
 
-    def getLabelCoords(self, size=None):
+    def getLabelCoords(
+            self, size: Optional[Tuple[int, int]] = None) -> Tuple[int, int]:
         use_size = self.default_size if size is None else size
         return int(use_size[0] / 4), \
                int(use_size[1] * 1 / 5)
 
-    def getValueCoords(self, size=None):
+    def getValueCoords(
+            self, size: Optional[Tuple[int, int]] = None) -> Tuple[int, int]:
         use_size = self.default_size if size is None else size
         return int(use_size[0] / 4), \
                int(use_size[1] * 3 / 5)
 
-    def getUnitCoords(self, size=None):
+    def getUnitCoords(
+            self, size: Optional[Tuple[int, int]] = None) -> Tuple[int, int]:
         use_size = self.default_size if size is None else size
         return int(use_size[0] / 4),\
                int(use_size[1] * 9 / 10)
@@ -132,19 +139,22 @@ class DisplayRectSettings:
 
 class UISettings:
     def __init__(self,
-                 fancy_button_settings=FancyButtonSettings(),
-                 simple_button_settings=SimpleButtonSettings(),
-                 display_rect_settings=DisplayRectSettings()):
+                 fancy_button_settings: FancyButtonSettings = FancyButtonSettings(),
+                 simple_button_settings: SimpleButtonSettings = SimpleButtonSettings(),
+                 display_rect_settings: DisplayRectSettings = DisplayRectSettings()):
 
         self.fancy_button_settings = fancy_button_settings
         self.simple_button_settings = simple_button_settings
         self.display_rect_settings = display_rect_settings
 
-    def set_fancy_button_settings(self, new_settings):
+    def set_fancy_button_settings(
+            self, new_settings: FancyButtonSettings) -> None:
         self.fancy_button_settings = new_settings
 
-    def set_simple_button_settings(self, new_settings):
+    def set_simple_button_settings(
+            self, new_settings: SimpleButtonSettings) -> None:
         self.simple_button_settings = new_settings
 
-    def set_display_rect_settings(self, new_settings):
+    def set_display_rect_settings(
+            self, new_settings: DisplayRectSettings) -> None: 
         self.display_rect_settings = new_settings
