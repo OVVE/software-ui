20e7ec2c8b75294a4ab88772d5f0823e4dc3000e
zmk5 <zkakish@gmail.com>
2020-04-02 20:37:36 -0700

    syntax fixes to generalize widget creation

--- ovve_ui/display/widgets.py
+++ ovve_ui/display/widgets.py
@@ -11,11 +11,10 @@ from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout
 
 def initializeHomeScreenWidget(window) -> None:
     """ Creates Home Screen for Widgets """
-    h_box_1 = QHBoxLayout()
-
-    v_box_1left = QVBoxLayout()
-    v_box_1mid = QVBoxLayout()
-    v_box_1right = QVBoxLayout()
+    h_box = QHBoxLayout()
+    v_box_left = QVBoxLayout()
+    v_box_mid = QVBoxLayout()
+    v_box_right = QVBoxLayout()
 
     window.mode_button_main = window.makeSimpleDisplayButton(
         window.settings.get_mode_display())
@@ -26,44 +25,23 @@ def initializeHomeScreenWidget(window) -> None:
     window.resp_rate_button_main.clicked.connect(lambda: window.display(2))
 
     window.minute_vol_button_main = window.makeFancyDisplayButton(
-        "Minute Volume",
-        window.settings.minute_volume,
-        "l/min",
-    )
+        "Minute Volume", window.settings.minute_volume, "l/min")
     window.minute_vol_button_main.clicked.connect(lambda: window.display(3))
 
     window.ie_button_main = window.makeFancyDisplayButton(
-        "I/E Ratio",
-        window.settings.get_ie_display(),
-        "l/min",
-    )
+        "I/E Ratio", window.settings.get_ie_display(), "l/min")
     window.ie_button_main.clicked.connect(lambda: window.display(4))
 
     window.peep_display_main = window.makeDisplayRect(
-        "PEEP",
-        5,
-        "cmH2O",
-    )
+        "PEEP", 5, "cmH2O")
     window.tv_insp_display_main = window.makeDisplayRect(
-        "TV Insp",
-        window.params.tv_insp,
-        "mL",
-    )
+        "TV Insp", window.params.tv_insp, "mL")
     window.tv_exp_display_main = window.makeDisplayRect(
-        "TV Exp",
-        window.params.tv_exp,
-        "mL",
-    )
+        "TV Exp", window.params.tv_exp, "mL")
     window.ppeak_display_main = window.makeDisplayRect(
-        "Ppeak",
-        window.params.ppeak,
-        "cmH2O",
-    )
+        "Ppeak", window.params.ppeak, "cmH2O")
     window.pplat_display_main = window.makeDisplayRect(
-        "Pplat",
-        window.params.pplat,
-        "cmH2O",
-    )
+        "Pplat", window.params.pplat, "cmH2O")
 
     axisStyle = {'color': 'black', 'font-size': '20pt'}
     graph_pen = pg.mkPen(width=5, color="b")
@@ -77,8 +55,7 @@ def initializeHomeScreenWidget(window) -> None:
     window.flow_graph = pg.PlotWidget()
     window.flow_graph.setFixedWidth(graph_width)
     window.flow_graph_line = window.flow_graph.plot(
-        window.tv_insp_data,
-        pen=graph_pen)  # shows Serial (tv_insp) data for now
+        window.tv_insp_data, pen=graph_pen)  # shows Serial (tv_insp) data for now
     window.flow_graph.setBackground("w")
     window.flow_graph.setMouseEnabled(False, False)
     flow_graph_left_axis = window.flow_graph.getAxis("left")
@@ -89,52 +66,48 @@ def initializeHomeScreenWidget(window) -> None:
 
     window.pressure_graph = pg.PlotWidget()
     window.pressure_graph.setFixedWidth(graph_width)
-    window.pressure_graph_line = window.pressure_graph.plot(indices,
-                                                        data,
-                                                        pen=graph_pen)
+    window.pressure_graph_line = window.pressure_graph.plot(
+        indices, data, pen=graph_pen)
     window.pressure_graph.setBackground("w")
     window.pressure_graph.setMouseEnabled(False, False)
     pressure_graph_left_axis = window.pressure_graph.getAxis("left")
-    pressure_graph_left_axis.setLabel("Pressure",
-                                        **axisStyle)  # TODO: Add units
+    pressure_graph_left_axis.setLabel("Pressure", **axisStyle)  # TODO: Add units
 
     window.volume_graph = pg.PlotWidget()
     window.volume_graph.setFixedWidth(graph_width)
-    window.pressure_graph_line = window.volume_graph.plot(indices,
-                                                        data,
-                                                        pen=graph_pen)
+    window.pressure_graph_line = window.volume_graph.plot(
+        indices, data, pen=graph_pen)
     window.volume_graph.setBackground("w")
     window.volume_graph.setMouseEnabled(False, False)
     window.pressure_graph_left_axis = window.volume_graph.getAxis("left")
-    window.pressure_graph_left_axis.setLabel("Volume",
-                                            **axisStyle)  # TODO: Add units
+    window.pressure_graph_left_axis.setLabel("Volume", **axisStyle)  # TODO: Add units
 
-    v_box_1left.addWidget(window.mode_button_main)
-    v_box_1left.addWidget(window.resp_rate_button_main)
-    v_box_1left.addWidget(window.minute_vol_button_main)
-    v_box_1left.addWidget(window.ie_button_main)
-    v_box_1left.addWidget(window.peep_display_main)
+    v_box_left.addWidget(window.mode_button_main)
+    v_box_left.addWidget(window.resp_rate_button_main)
+    v_box_left.addWidget(window.minute_vol_button_main)
+    v_box_left.addWidget(window.ie_button_main)
+    v_box_left.addWidget(window.peep_display_main)
 
-    v_box_1mid.addWidget(window.flow_graph)
-    v_box_1mid.addWidget(window.pressure_graph)
-    v_box_1mid.addWidget(window.volume_graph)
+    v_box_mid.addWidget(window.flow_graph)
+    v_box_mid.addWidget(window.pressure_graph)
+    v_box_mid.addWidget(window.volume_graph)
 
-    v_box_1right.addWidget(window.tv_insp_display_main)
-    v_box_1right.addWidget(window.tv_exp_display_main)
-    v_box_1right.addWidget(window.ppeak_display_main)
-    v_box_1right.addWidget(window.pplat_display_main)
+    v_box_right.addWidget(window.tv_insp_display_main)
+    v_box_right.addWidget(window.tv_exp_display_main)
+    v_box_right.addWidget(window.ppeak_display_main)
+    v_box_right.addWidget(window.pplat_display_main)
 
-    h_box_1.addLayout(v_box_1left)
-    h_box_1.addLayout(v_box_1mid)
-    h_box_1.addLayout(v_box_1right)
-    window.page["1"].setLayout(h_box_1)
+    h_box.addLayout(v_box_left)
+    h_box.addLayout(v_box_mid)
+    h_box.addLayout(v_box_right)
+    window.page["1"].setLayout(h_box)
 
 def initializeModeWidget(window) -> None:
     """ Creates Mode Widget """
-    v_box_2 = QVBoxLayout()
-    h_box_2top = QHBoxLayout()
-    h_box_2middle = QHBoxLayout()
-    h_box_2bottom = QHBoxLayout()
+    v_box = QVBoxLayout()
+    h_box_top = QHBoxLayout()
+    h_box_mid= QHBoxLayout()
+    h_box_bot = QHBoxLayout()
 
     mode_change = window.makeSimpleDisplayButton("CHANGE MODE")
     mode_apply = window.makeSimpleDisplayButton("APPLY")
@@ -146,28 +119,25 @@ def initializeModeWidget(window) -> None:
     mode_cancel.clicked.connect(window.cancelChange)
 
     window.mode_page_rect = window.makeDisplayRect(
-        "Mode",
-        window.local_settings.get_mode_display(),
-        "",
-        size=(500, 200))
+        "Mode", window.local_settings.get_mode_display(), "", size=(500, 200))
 
-    h_box_2top.addWidget(window.mode_page_rect)
-    h_box_2middle.addWidget(mode_change)
-    h_box_2bottom.addWidget(mode_apply)
-    h_box_2bottom.addWidget(mode_cancel)
+    h_box_top.addWidget(window.mode_page_rect)
+    h_box_mid.addWidget(mode_change)
+    h_box_bot.addWidget(mode_apply)
+    h_box_bot.addWidget(mode_cancel)
 
-    v_box_2.addLayout(h_box_2top)
-    v_box_2.addLayout(h_box_2middle)
-    v_box_2.addLayout(h_box_2bottom)
+    v_box.addLayout(h_box_top)
+    v_box.addLayout(h_box_mid)
+    v_box.addLayout(h_box_bot)
 
-    window.page["2"].setLayout(v_box_2)
+    window.page["2"].setLayout(v_box)
 
 def initializeRespitoryRateWidget(window) -> None:
     """ Creates Respitory Rate Widget """
-    v_box_3 = QVBoxLayout()
-    h_box_3top = QHBoxLayout()
-    h_box_3mid = QHBoxLayout()
-    h_box_3bottom = QHBoxLayout()
+    v_box = QVBoxLayout()
+    h_box_top = QHBoxLayout()
+    h_box_mid = QHBoxLayout()
+    h_box_bot = QHBoxLayout()
 
     window.resp_rate_page_rect = window.makeDisplayRect(
         "Resp. Rate",
@@ -187,24 +157,24 @@ def initializeRespitoryRateWidget(window) -> None:
     resp_rate_apply.clicked.connect(window.commitRespRate)
     resp_rate_cancel.clicked.connect(window.cancelChange)
 
-    h_box_3top.addWidget(window.resp_rate_page_rect)
-    h_box_3mid.addWidget(resp_rate_increment_button)
-    h_box_3mid.addWidget(resp_rate_decrement_button)
-    h_box_3bottom.addWidget(resp_rate_apply)
-    h_box_3bottom.addWidget(resp_rate_cancel)
+    h_box_top.addWidget(window.resp_rate_page_rect)
+    h_box_mid.addWidget(resp_rate_increment_button)
+    h_box_mid.addWidget(resp_rate_decrement_button)
+    h_box_bot.addWidget(resp_rate_apply)
+    h_box_bot.addWidget(resp_rate_cancel)
 
-    v_box_3.addLayout(h_box_3top)
-    v_box_3.addLayout(h_box_3mid)
-    v_box_3.addLayout(h_box_3bottom)
+    v_box.addLayout(h_box_top)
+    v_box.addLayout(h_box_mid)
+    v_box.addLayout(h_box_bot)
 
-    window.page["3"].setLayout(v_box_3)
+    window.page["3"].setLayout(v_box)
 
 def initializeMinuteVolumeWidget(window):
     """ Creates Minute Volume Widget """
-    v_box_4 = QVBoxLayout()
-    h_box_4top = QHBoxLayout()
-    h_box_4mid = QHBoxLayout()
-    h_box_4bottom = QHBoxLayout()
+    v_box = QVBoxLayout()
+    h_box_top = QHBoxLayout()
+    h_box_mid = QHBoxLayout()
+    h_box_bot = QHBoxLayout()
 
     window.minute_vol_page_rect = window.makeDisplayRect(
         "Minute Volume",
@@ -224,24 +194,24 @@ def initializeMinuteVolumeWidget(window):
     minute_vol_apply.clicked.connect(window.commitMinuteVol)
     minute_vol_cancel.clicked.connect(window.cancelChange)
 
-    h_box_4top.addWidget(window.minute_vol_page_rect)
-    h_box_4mid.addWidget(minute_vol_increment_button)
-    h_box_4mid.addWidget(minute_vol_decrement_button)
-    h_box_4bottom.addWidget(minute_vol_apply)
-    h_box_4bottom.addWidget(minute_vol_cancel)
+    h_box_top.addWidget(window.minute_vol_page_rect)
+    h_box_mid.addWidget(minute_vol_increment_button)
+    h_box_mid.addWidget(minute_vol_decrement_button)
+    h_box_bot.addWidget(minute_vol_apply)
+    h_box_bot.addWidget(minute_vol_cancel)
 
-    v_box_4.addLayout(h_box_4top)
-    v_box_4.addLayout(h_box_4mid)
-    v_box_4.addLayout(h_box_4bottom)
+    v_box.addLayout(h_box_top)
+    v_box.addLayout(h_box_mid)
+    v_box.addLayout(h_box_bot)
 
-    window.page["4"].setLayout(v_box_4)
+    window.page["4"].setLayout(v_box)
 
 def initializeIERatioWidget(window):
     """ Creates i/e Ratio Widget """
-    v_box_5 = QVBoxLayout()
-    h_box_5top = QHBoxLayout()
-    h_box_5mid = QHBoxLayout()
-    h_box_5bottom = QHBoxLayout()
+    v_box = QVBoxLayout()
+    h_box_top = QHBoxLayout()
+    h_box_mid = QHBoxLayout()
+    h_box_bot = QHBoxLayout()
 
     window.ie_page_rect = window.makeDisplayRect(
         "I/E Ratio", window.settings.get_ie_display(), "", size=(500, 200))
@@ -268,17 +238,17 @@ def initializeIERatioWidget(window):
     ie_apply.clicked.connect(window.commitIERatio)
     ie_cancel.clicked.connect(window.cancelChange)
 
-    h_box_5top.addWidget(window.ie_page_rect)
-    h_box_5mid.addWidget(ie_change_0)
-    h_box_5mid.addWidget(ie_change_1)
-    h_box_5mid.addWidget(ie_change_2)
-    h_box_5mid.addWidget(ie_change_3)
+    h_box_top.addWidget(window.ie_page_rect)
+    h_box_mid.addWidget(ie_change_0)
+    h_box_mid.addWidget(ie_change_1)
+    h_box_mid.addWidget(ie_change_2)
+    h_box_mid.addWidget(ie_change_3)
 
-    h_box_5bottom.addWidget(ie_apply)
-    h_box_5bottom.addWidget(ie_cancel)
+    h_box_bot.addWidget(ie_apply)
+    h_box_bot.addWidget(ie_cancel)
 
-    v_box_5.addLayout(h_box_5top)
-    v_box_5.addLayout(h_box_5mid)
-    v_box_5.addLayout(h_box_5bottom)
+    v_box.addLayout(h_box_top)
+    v_box.addLayout(h_box_mid)
+    v_box.addLayout(h_box_bot)
 
-    window.page["5"].setLayout(v_box_5)
+    window.page["5"].setLayout(v_box)
