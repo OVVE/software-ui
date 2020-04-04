cd081fbcd9d3b2b190f2c069c7dc70778884dc92
Brian Woodruff <brian.e.woodruff@gmail.com>
2020-04-03 21:08:07 -0700

    Make simulator respond to start / stop

--- ovve_ui/utils/comms_simulator.py
+++ ovve_ui/utils/comms_simulator.py
@@ -28,30 +28,31 @@ class CommsSimulator():
         params_dict = json.loads(params_str)
 
         while not self.done:
-            params_dict['run_state'] = self.settings.run_state
-            params_dict['seq_num'] = self.seqnum
-            params_dict['packet_version'] = self.packet_version
-            params_dict['mode'] = self.settings.mode
-            params_dict['resp_rate_meas'] = self.settings.resp_rate
-            params_dict['resp_rate_set'] = self.settings.resp_rate
-            params_dict['tv_meas'] = self.settings.tv
-            params_dict['tv_set'] = self.settings.tv
-            params_dict['ie_ratio_meas'] = self.settings.ie_ratio
-            params_dict['ie_ratio_set'] = self.settings.ie_ratio
-            params_dict['peep'] = random.randrange(3, 6)
-            params_dict['ppeak'] = random.randrange(15, 20)
-            params_dict['pplat'] = random.randrange(15, 20)
-            params_dict['pressure'] = random.randrange(15, 20)
-            params_dict['flow'] = random.randrange(15, 20)
-            params_dict['tv_insp'] = random.randrange(475, 575)
-            params_dict['tv_exp'] = random.randrange(475, 575)
-            params_dict['tv_min'] = random.randrange(475, 575)
-            params_dict['tv_min'] = random.randrange(475, 575)
-            params_dict['control_state'] = 0
-            params_dict['battery_level'] = 255
-            self.comms_adapter.update_params(params_dict)
-
-            self.seqnum += 1
+            if self.settings.run_state > 0:
+                params_dict['run_state'] = self.settings.run_state
+                params_dict['seq_num'] = self.seqnum
+                params_dict['packet_version'] = self.packet_version
+                params_dict['mode'] = self.settings.mode
+                params_dict['resp_rate_meas'] = self.settings.resp_rate
+                params_dict['resp_rate_set'] = self.settings.resp_rate
+                params_dict['tv_meas'] = self.settings.tv
+                params_dict['tv_set'] = self.settings.tv
+                params_dict['ie_ratio_meas'] = self.settings.ie_ratio
+                params_dict['ie_ratio_set'] = self.settings.ie_ratio
+                params_dict['peep'] = random.randrange(3, 6)
+                params_dict['ppeak'] = random.randrange(15, 20)
+                params_dict['pplat'] = random.randrange(15, 20)
+                params_dict['pressure'] = random.randrange(15, 20)
+                params_dict['flow'] = random.randrange(15, 20)
+                params_dict['tv_insp'] = random.randrange(475, 575)
+                params_dict['tv_exp'] = random.randrange(475, 575)
+                params_dict['tv_min'] = random.randrange(475, 575)
+                params_dict['tv_min'] = random.randrange(475, 575)
+                params_dict['control_state'] = 0
+                params_dict['battery_level'] = 255
+                self.comms_adapter.update_params(params_dict)
+                self.seqnum += 1
+            
             time.sleep(1)
 
 
