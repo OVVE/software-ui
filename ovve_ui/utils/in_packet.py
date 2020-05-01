accafc6fa4bcf1e3b7ef9c4c8546a8a3d924d756
Mcinnis <russ.mcinnis@intel.com>
2020-05-01 15:42:38 -0700

    update structs for packet version 3

--- ovve_ui/utils/in_packet.py
+++ ovve_ui/utils/in_packet.py
@@ -41,27 +41,37 @@ class InPacket():
     def from_bytes(self, byteData: bytes) -> None:
         self.data['sequence_count']=int.from_bytes(byteData[0:2], byteorder='little')
         self.data['packet_version']=byteData[2]
-        self.data['run_state'] = byteData[3] & (1 << 7)
-        self.data['mode_value']= byteData[3] & 0x7F
-        self.data['respiratory_rate_measured']=int.from_bytes(byteData[4:8], byteorder='little')
-        self.data['respiratory_rate_set']=int.from_bytes(byteData[8:12], byteorder='little')
-        self.data['tidal_volume_measured']=int.from_bytes(byteData[12:16], byteorder='little', signed=True)
-        self.data['tidal_volume_set']=int.from_bytes(byteData[16:20], byteorder='little', signed=True)
-        self.data['ie_ratio_measured']=int.from_bytes(byteData[20:24], byteorder='little')
-        self.data['ie_ratio_set']=int.from_bytes(byteData[24:28], byteorder='little')
-        self.data['peep_value_measured']=int.from_bytes(byteData[28:32], byteorder='little', signed=True)
-        self.data['peak_pressure_measured']=int.from_bytes(byteData[32:36], byteorder='little', signed=True)
-        self.data['plateau_value_measured']=int.from_bytes(byteData[36:40], byteorder='little', signed=True)
-        self.data['pressure_measured']=int.from_bytes(byteData[40:44], byteorder='little', signed=True)
-        self.data['flow_measured']=int.from_bytes(byteData[44:48], byteorder='little', signed=True)
-        self.data['volume_in_measured']=int.from_bytes(byteData[48:52], byteorder='little', signed=True)
-        self.data['volume_out_measured']=int.from_bytes(byteData[52:56], byteorder='little', signed=True)
-        self.data['volume_rate_measured']=int.from_bytes(byteData[56:60], byteorder='little', signed=True)
-        self.data['control_state']=byteData[60]
-        self.data['battery_level']=byteData[61]
-        self.data['reserved']=int.from_bytes(byteData[62:64], byteorder='little')
-        self.data['alarm_bits']=int.from_bytes(byteData[64:68], byteorder='little')
-        self.data['crc']=int.from_bytes(byteData[68:], byteorder='little')
+        self.data['packet_type']=byteData[3]        
+        self.data['mode_value']=byteData[4]
+        self.data['control_state']=byteData[5] & 0x1F
+        self.data['run_state'] = byteData[5] & (1 << 7)
+        self.data['battery_charge'] = byteData[6] & (1 << 7)
+        self.data['battery_level'] = byteData[6] & 0x7F
+        self.data['reserved'] = byteData[7]
+        self.data['respiratory_rate_set']=int.from_bytes(byteData[8:10], byteorder='little')
+        self.data['respiratory_rate_measured']=int.from_bytes(byteData[10:12], byteorder='little')
+        self.data['tidal_volume_set']=int.from_bytes(byteData[12:14], byteorder='little', signed=True)
+        self.data['tidal_volume_measured']=int.from_bytes(byteData[14:16], byteorder='little', signed=True)
+        self.data['ie_ratio_set']=int.from_bytes(byteData[16:18], byteorder='little')
+        self.data['ie_ratio_measured']=int.from_bytes(byteData[18:20], byteorder='little')
+        self.data['peep_value_measured']=int.from_bytes(byteData[20:22], byteorder='little', signed=True)
+        self.data['peak_pressure_measured']=int.from_bytes(byteData[22:24], byteorder='little', signed=True)
+        self.data['plateau_value_measured']=int.from_bytes(byteData[24:26], byteorder='little', signed=True)
+        self.data['pressure_set']=int.from_bytes(byteData[26:28], byteorder='little', signed=True)
+        self.data['pressure_measured']=int.from_bytes(byteData[28:30], byteorder='little', signed=True)
+        self.data['flow_measured']=int.from_bytes(byteData[30:32], byteorder='little', signed=True)
+        self.data['volume_in_measured']=int.from_bytes(byteData[32:34], byteorder='little', signed=True)
+        self.data['volume_out_measured']=int.from_bytes(byteData[34:36], byteorder='little', signed=True)
+        self.data['volume_rate_measured']=int.from_bytes(byteData[36:38], byteorder='little', signed=True)
+        self.data['high_pressure_limit_set']=int.from_bytes(byteData[38:40], byteorder='little', signed=True)
+        self.data['low_pressure_limit_set']=int.from_bytes(byteData[40:42], byteorder='little', signed=True)
+        self.data['high_volume_limit_set']=int.from_bytes(byteData[42:44], byteorder='little', signed=True)
+        self.data['low_volume_limit_set']=int.from_bytes(byteData[44:46], byteorder='little', signed=True)
+        self.data['high_respiratory_rate_limit_set']=int.from_bytes(byteData[46:48], byteorder='little', signed=True)
+        self.data['low_respiratory_rate_limit_set']=int.from_bytes(byteData[48:50], byteorder='little', signed=True)
+        
+        self.data['alarm_bits']=int.from_bytes(byteData[50:52], byteorder='little')
+        self.data['crc']=int.from_bytes(byteData[52:], byteorder='little')
 
 
     def to_params(self) -> Params:
