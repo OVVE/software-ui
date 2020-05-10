cdcd535d6eeb49f4c586c01b06042ecbaf1e5207
JanS <jan@stumpf.eu>
2020-05-10 21:40:00 +0200

    New serial protocol with headers
    Implemented new serial protocol V3.5 with headers and no timing
    constraints
    Now running baudrate of 125000 baud (-> no fractional BR divider at
    16Mhz in Atmel, therefore non-standard for best performance)
    Baudrate tested up to 500.000baud without errors
    Intensive statistics for link
    Read to add debugging
    Initial testing and stress testing completed

--- ovve_ui/utils/in_packet.py
+++ ovve_ui/utils/in_packet.py
@@ -3,71 +3,65 @@ from utils.units import Units
 
 class InPacket():
     def __init__(self) -> None:
-        self.data={'sequence_count': 0,            # bytes  0- 1 - rpi unsigned short int
-                    'packet_version': 0,            # byte 2      - rpi unsigned char
-                    'mode_value': 0,                # byte 3      - rpi unsigned char
-                    'respiratory_rate_measured': 0, # bytes 4 - 7 - rpi unsigned int
-                    'respiratory_rate_set': 0,      # bytes 8 - 11
-                    'tidal_volume_measured': 0,     # bytes 12 - 15
-                    'tidal_volume_set': 0,          # bytes 16 - 19
-                    'ie_ratio_measured': 0,         # bytes 20 - 23
-                    'ie_ratio_set': 0,              # bytes 24 - 27
-                    'peep_value_measured': 0,       # bytes 28 - 31
-                    'peak_pressure_measured': 0,    # bytes 32 - 35
-                    'plateau_value_measured': 0,    # bytes 36 - 39
-                    'pressure_measured': 0,         # bytes 40 - 43
-                    'flow_measured': 0,             # bytes 44 - 47
-                    'volume_in_measured': 0,        # bytes 48 - 51
-                    'volume_out_measured': 0,       # bytes 52 - 55
-                    'volume_rate_measured': 0,      # bytes 56 - 59
-                    'control_state': 0,             # byte 60       - rpi unsigned char
-                    'battery_level': 0,             # byte 61
-                    'reserved': 0,                  # bytes 62 - 63 - rpi unsigned int
-                    'alarm_bits': 0,                # bytes 64 - 67
-                    'crc': 0,                       # bytes 68 - 69
-                    'run_state': 0                  # Calculated from mode value
-                    }
+        self.data={ 'mode_value': 0,                
+                    'respiratory_rate_measured': 0, 
+                    'respiratory_rate_set': 0,      
+                    'tidal_volume_measured': 0,     
+                    'tidal_volume_set': 0,          
+                    'ie_ratio_measured': 0,         
+                    'ie_ratio_set': 0,              
+                    'peep_value_measured': 0,       
+                    'peak_pressure_measured': 0,    
+                    'plateau_value_measured': 0,    
+                    'pressure_measured': 0,         
+                    'flow_measured': 0,             
+                    'volume_in_measured': 0,        
+                    'volume_out_measured': 0,       
+                    'volume_rate_measured': 0,      
+                    'control_state': 0,             
+                    'battery_level': 0,             
+                    'reserved': 0,                  
+                    'alarm_bits': 0,                
+                    'run_state': 0                  
+                 }
 
 
     # byteData must have already been checked for proper length and crc
     def from_bytes(self, byteData: bytes) -> None:
-        self.data['sequence_count']=int.from_bytes(byteData[0:2], byteorder='little')
-        self.data['packet_version']=byteData[2]
-        self.data['run_state'] = byteData[3] & (1 << 7)
-        self.data['mode_value']= byteData[3] & 0x7F
-        self.data['respiratory_rate_measured']=int.from_bytes(byteData[4:8], byteorder='little')
-        self.data['respiratory_rate_set']=int.from_bytes(byteData[8:12], byteorder='little')
-        self.data['tidal_volume_measured']=int.from_bytes(byteData[12:16], byteorder='little', signed=True)
-        self.data['tidal_volume_set']=int.from_bytes(byteData[16:20], byteorder='little', signed=True)
+        self.data['run_state'] = byteData[0] & (1 << 7)
+        self.data['mode_value']= byteData[0] & 0x7F
+        self.data['respiratory_rate_measured']=int.from_bytes(byteData[1:5], byteorder='little')
+        self.data['respiratory_rate_set']=int.from_bytes(byteData[5:9], byteorder='little')
+        self.data['tidal_volume_measured']=int.from_bytes(byteData[9:13], byteorder='little', signed=True)
+        self.data['tidal_volume_set']=int.from_bytes(byteData[13:17], byteorder='little', signed=True)
         
-        ie_measured_fixed = int.from_bytes(byteData[20:24], byteorder='little')
+        ie_measured_fixed = int.from_bytes(byteData[17:21], byteorder='little')
         ie_measured_fraction = self.ie_fixed_to_fraction(ie_measured_fixed)
         self.data['ie_ratio_measured'] = ie_measured_fraction
 
-        ie_set_fixed = int.from_bytes(byteData[24:28], byteorder='little')
+        ie_set_fixed = int.from_bytes(byteData[21:25], byteorder='little')
         ie_set_fraction = self.ie_fixed_to_fraction(ie_set_fixed)
         self.data['ie_ratio_set'] = ie_set_fraction
 
-        self.data['peep_value_measured']=int.from_bytes(byteData[28:32], byteorder='little', signed=True)
-        self.data['peak_pressure_measured']=int.from_bytes(byteData[32:36], byteorder='little', signed=True)
-        self.data['plateau_value_measured']=int.from_bytes(byteData[36:40], byteorder='little', signed=True)
-        self.data['pressure_measured']=int.from_bytes(byteData[40:44], byteorder='little', signed=True)
-        self.data['flow_measured']=int.from_bytes(byteData[44:48], byteorder='little', signed=True)
-        self.data['volume_in_measured']=int.from_bytes(byteData[48:52], byteorder='little', signed=True)
-        self.data['volume_out_measured']=int.from_bytes(byteData[52:56], byteorder='little', signed=True)
-        self.data['volume_rate_measured']=int.from_bytes(byteData[56:60], byteorder='little', signed=True)
-        self.data['control_state']=byteData[60]
-        self.data['battery_level']=byteData[61]
-        self.data['reserved']=int.from_bytes(byteData[62:64], byteorder='little')
-        self.data['alarm_bits']=int.from_bytes(byteData[64:68], byteorder='little')
-        self.data['crc']=int.from_bytes(byteData[68:], byteorder='little')
+        self.data['peep_value_measured']=int.from_bytes(byteData[25:29], byteorder='little', signed=True)
+        self.data['peak_pressure_measured']=int.from_bytes(byteData[29:33], byteorder='little', signed=True)
+        self.data['plateau_value_measured']=int.from_bytes(byteData[33:37], byteorder='little', signed=True)
+        self.data['pressure_measured']=int.from_bytes(byteData[37:41], byteorder='little', signed=True)
+        self.data['flow_measured']=int.from_bytes(byteData[41:45], byteorder='little', signed=True)
+        self.data['volume_in_measured']=int.from_bytes(byteData[45:49], byteorder='little', signed=True)
+        self.data['volume_out_measured']=int.from_bytes(byteData[49:53], byteorder='little', signed=True)
+        self.data['volume_rate_measured']=int.from_bytes(byteData[53:57], byteorder='little', signed=True)
+        self.data['control_state']=byteData[57]
+        self.data['battery_level']=byteData[58]
+        self.data['reserved']=int.from_bytes(byteData[59:61], byteorder='little')
+        self.data['alarm_bits']=int.from_bytes(byteData[61:65], byteorder='little')
 
 
-    def to_params(self) -> Params:
+    def to_params(self,sequenceNo) -> Params:
         params = Params()
         params.run_state = self.data['run_state']
-        params.seq_num = self.data['sequence_count']
-        params.packet_version = self.data['packet_version']
+        params.seq_num = sequenceNo
+        params.packet_version = 4
         params.mode = self.data['mode_value']
         params.resp_rate_meas = self.data['respiratory_rate_measured']
         params.resp_rate_set = self.data['respiratory_rate_set']
--- ovve_ui/utils/out_packet.py
+++ ovve_ui/utils/out_packet.py
@@ -5,14 +5,13 @@ from utils.crc import CRC
 class OutPacket():
     def __init__(self) -> None:
         self.crc = CRC()
-        self.data = {'sequence_count': 0,               # bytes 0 - 1 - rpi unsigned short int
-            'packet_version': 1,                         # byte 2      - rpi unsigned char
+        self.data = {
             'mode_value': 0,                             # byte 3      - rpi unsigned char
             'respiratory_rate_set': 0, # bytes 5 - 8 - rpi unsigned int
             'tidal_volume_set':  0,         # bytes 8 - 11
             'ie_ratio_set':  0,             # bytes 12 - 15
             'alarm_bits':   0,              # bytes 16 - 19
-            'crc':   0 }                    # bytes 20 - 21 - rpi unsigned short int 
+            }                    # bytes 20 - 21 - rpi unsigned short int 
 
     def to_bytes(self) -> bytes:
         # create the return packet
@@ -20,8 +19,6 @@ class OutPacket():
         cmd_byteData = b""
         #packet_version = 1
 
-        cmd_byteData += bytes(self.data['sequence_count'].to_bytes(2, endian))
-        cmd_byteData += bytes(self.data['packet_version'].to_bytes(1, endian))
         cmd_byteData += bytes(self.data['mode_value'].to_bytes(1, endian))
         cmd_byteData += bytes(self.data['respiratory_rate_set'].to_bytes(4, endian))
         cmd_byteData += bytes(self.data['tidal_volume_set'].to_bytes(4, endian, signed=True))
@@ -29,14 +26,6 @@ class OutPacket():
            
         # TO DO set alarmbits correctly if sequence or CRC failed
         cmd_byteData += bytes(self.data['alarm_bits'].to_bytes(4, endian))
-
-        #Get CRC
-        calcCRC = self.crc.crccitt(cmd_byteData.hex())
-        # flip the bits - note that this will be 32 bit hex - do we will only send the first 2 later
-        CRCtoSend = struct.pack('<Q', int(calcCRC, base=16))
-
-        #send only 2 bytes
-        cmd_byteData += CRCtoSend[0:2]
 
         return cmd_byteData
         
