11666417f0a44f4c15215625d6377b8929f3c105
Brian Woodruff <brian.e.woodruff@gmail.com>
2020-05-13 16:58:08 -0700

    Initial implementation of alarm handler, types, messages, queue

---
+++ ovve_ui/alarmtest.py
@@ -0,0 +1,69 @@
+import time
+from PyQt5 import QtCore
+from PyQt5.QtCore import pyqtSignal
+from utils.Alarm import *
+
+'''
+  Simulates what should happen in comms handler
+'''
+class AlarmEmitter(QtCore.QThread):
+    new_alarm_signal = pyqtSignal(bytearray)
+
+    def __init__(self, handler: AlarmHandler):
+        super().__init__()
+        self.handler = handler
+        self.new_alarm_signal.connect(self.handler.handle_alarms)
+        self.handler.acknowledge_alarm_signal.connect(self.handle_ack)
+        self.start()
+
+    def handle_ack(self):
+        print("Ack received from alarm handler")
+
+    def run(self):
+        count = 0
+        while True:  
+            print("Emitting an alarm")
+            alarm_bytes = bytearray(5)
+            alarm_bytes[4] = count
+            self.new_alarm_signal.emit(alarm_bytes)
+            count += 1
+            time.sleep(1)
+            
+'''
+  Simulates what should happen in UI
+'''
+class AlarmConsumer(QtCore.QThread):
+    def __init__(self, handler: AlarmHandler):
+        super().__init__()
+        self.handler = handler
+        self.start()
+
+    def run(self):
+        while True:  
+            
+            try:
+                print("Get an alarm from the queue")
+                highest_alarm = self.handler.get_highest_priority_alarm()
+                print("Consumer acknowledging the alarm")
+                self.handler.acknowledge_alarm(highest_alarm)
+            except queue.Empty:
+                pass
+
+            # OR, you can test for empty first
+            
+            # if self.handler.is_alarm_pending():
+            #     print("Get an alarm from the queue")
+            #     highest_alarm = self.handler.get_highest_priority_alarm()
+            #     print("Acknowledge the alarm")
+            #     self.handler.acknowledge_alarm(highest_alarm)
+        
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    import sys
+    app = QtCore.QCoreApplication(sys.argv)
+    handler = AlarmHandler()
+    alarmemitter = AlarmEmitter(handler)
+    alarmconsumer = AlarmConsumer(handler)
+    sys.exit(app.exec_())
--- ovve_ui/utils/Alarm.py
+++ ovve_ui/utils/Alarm.py
@@ -1,5 +1,87 @@
+import time
+from enum import Enum
+from queue import PriorityQueue
+from typing import List
+from PyQt5.QtCore import pyqtSignal
+from PyQt5 import QtCore
+
+class AlarmType(Enum):
+    LOW_PRESSURE = 1
+    HIGH_PRESSURE = 2
+
+class AlarmMessages():
+    def __init__(self) -> None:
+        self.messages : dict = {
+            AlarmType.LOW_PRESSURE: "Low pressure warning!",
+            AlarmTyp.HIGH_PRESSURE: "High pressure warning!"
+        }
+
 class Alarm():
-    def __init__(self, time, alarm_type, priority: int):
-        self.time = time
+    def __init__(self, alarm_type: AlarmType):
+        self.timefired = time.time()
         self.alarm_type = alarm_type
-        self.priority = priority
+
+class AlarmQueue(PriorityQueue):
+    priorities : dict = {
+        AlarmType.LOW_PRESSURE: 100,
+        AlarmType.HIGH_PRESSURE: 200
+    }
+
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def put(self, alarm: Alarm):
+        # TODO: Check and see if alarm is already in the queue 
+        # before adding a new entry
+        super().put(self.priorities.get(alarm.alarm_type), alarm)
+
+
+class AlarmHandler(QtCore.QObject):
+    acknowledge_alarm_signal = pyqtSignal(bytearray)
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.alarm_queue = AlarmQueue()
+        
+    '''
+     This function should be connected to a signal emitted
+     by the comms handler when alarm bits are received
+    '''
+    def handle_alarms(self, alarm_bytes: bytearray) -> None:
+        print("Alarm handler got alarm bytes " + str(alarm_bytes))
+        
+        # TODO: Parse bytes and enqueue alarms
+        # parse alarm bits and add alarms to the priority queue
+        # Ex.  e got a low pressure alarm
+        self.enqueue_alarm(AlarmType.LOW_PRESSURE)
+
+    '''
+     This function should be called by the UI when it
+     acknowledges an alarm.  It emits a signal that is
+     handled by comms to send the bytes back to the MCU
+    '''
+    def acknowledge_alarm(self, alarm: Alarm):
+        # Create a byte array for acknowledged alarms
+        # Emit a signal that will be received by the comms handler
+
+        # TODO: Convert alarm to bytes
+        ackbytes = bytearray(5)
+        self.acknowledge_alarm_signal.emit(ackbytes)
+
+
+    '''
+      This function is called by the UI to retrieve the
+      highest priority alarm in the queue
+    '''
+    def get_highest_priority_alarm(self) -> Alarm:
+        return self.alarm_queue.get()  
+
+    def enqueue_alarm(self, alarm_type: AlarmType) -> None:
+        alarm = Alarm(alarm_type)
+        self.alarm_queue.put(alarm)
+
+    def is_alarm_pending(self) -> bool:
+        return self.alarm_queue.empty()
+
+
+    
