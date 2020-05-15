a9cb3f17736dfa7e895bdc0e31fa9cd0b43f33f0
Avi Gupta <avigupta7@gmail.com>
2020-05-15 13:16:16 -0700

    Added proper file changes – logging enabled on this commit for testing purposes

--- ovve_ui/ovve_ui.py
+++ ovve_ui/ovve_ui.py
@@ -119,7 +119,7 @@ class MainWindow(QWidget):
 
         # Set the filehandler to log raw packets, warnings, and higher
         # Raw packets are logged at custom log level 25, just above INFO
-        self.fh.setLevel(25)
+        self.fh.setLevel(logging.INFO)
 
         # Log to console with human-readable output
         ch = logging.StreamHandler()
@@ -513,13 +513,17 @@ class MainWindow(QWidget):
         self.main_patient_label.setText( f"Current Patient: Patient {self.patient_id_display}")
 
 
+        self.logpath = os.path.join("/tmp", "ovve_logs", str(self.patient_id))
+        if not os.path.exists(self.logpath):
+            os.makedirs(self.logpath)
+
         self.logfileroot = os.path.join(self.logpath, str(self.patient_id) + ".log")
         self.logger.removeHandler(self.fh)
         self.fh = TimedRotatingFileHandler(self.logfileroot,
                                       when='H',
                                       interval=1,
                                       backupCount=336)
-        self.fh.setLevel(25)
+        self.fh.setLevel(logging.INFO)
         self.fh.setFormatter(self.formatter)
         self.logger.addHandler(self.fh)
 
