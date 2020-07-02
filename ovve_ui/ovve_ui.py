2cbbadcb77fecc9fd108ed8afeeb11aa26bd2ce3
Brian Woodruff <brian.e.woodruff@gmail.com>
2020-07-02 08:54:42 -0700

    Restore estop code

--- ovve_ui/ovve_ui.py
+++ ovve_ui/ovve_ui.py
@@ -47,7 +47,7 @@ from display.widgets import (initializeHomeScreenWidget, initializeModeWidget,
 
 from utils.params import Params
 from utils.settings import Settings
-from utils.Alarm import Alarm, AlarmHandler
+from utils.Alarm import Alarm, AlarmHandler, AlarmType
 from utils.comms_simulator import CommsSimulator
 from utils.comms_link import CommsLink
 from utils.ranges import Ranges
@@ -319,9 +319,10 @@ class MainWindow(QWidget):
 
     def update_ui_params(self, params: Params) -> None:
         self.params = params
+        self.logger.info(self.params.to_JSON())
+        self.update_ui_alarms()
+
         if self.params.run_state > 0:
-            self.logger.info(self.params.to_JSON())
-            self.update_ui_alarms()
             self.updateMainDisplays()
             self.updateGraphs()
 
@@ -333,6 +334,9 @@ class MainWindow(QWidget):
                 self.shown_alarm = self.alarm_handler.get_highest_priority_alarm(
                 )
                 self.showAlarm()
+
+                if (self.shown_alarm.alarm_type == AlarmType.ESTOP_PRESSED):
+                    self.setStartStop(0)
 
             elif not self.shown_alarm.isSamePrior(
                     self.alarm_handler.get_highest_priority_alarm()):
@@ -547,15 +551,21 @@ class MainWindow(QWidget):
         self.ie_ratio_page_value_label.setText(
             self.get_ie_ratio_display(self.local_settings.ie_ratio_enum))
 
+    def setStartStop(self, run_state: int) -> None:
+        if run_state == 1:
+            self.start_stop_button_main.updateValue("STOP")
+        else:
+             self.start_stop_button_main.updateValue("START")
+             self.start_stop_button_main.button_settings = SimpleButtonSettings()
+        self.settings.run_state = run_state
+        self.passChanges()
+            
     def changeStartStop(self) -> None:
         if self.settings.run_state == 0:
-            self.settings.run_state = 1
-            self.start_stop_button_main.updateValue("STOP")
-            self.passChanges()
-
-        elif self.settings.run_state == 1:
+            self.setStartStop(1)
+        else:
             self.confirmStop()
-
+    
     def generateNewPatientID(self) -> None:
         self.new_patient_id = uuid.uuid4()
         self.new_patient_id_display += 1
@@ -568,9 +578,7 @@ class MainWindow(QWidget):
         self.display(7)
 
     def stopVentilation(self) -> None:
-        self.settings.run_state = 0
-        self.start_stop_button_main.updateValue("START")
-        self.passChanges()
+        self.setStartStop(0)
         self.display(0)
 
     def commitMode(self) -> None:
